89405898fbf087c230ada0819ed561c7b286dee1
Анна Петросян <apetrosia@sfedu.ru>
2022-10-14 21:47:56 +0300

    First commit

---
+++ Assets/Scripts/GameResources/GameResources.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameResources : MonoBehaviour
+{
+    private static GameResources instance;
+
+    public static GameResources Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = Resources.Load<GameResources>("GameResources");
+            return instance;
+        }
+    }
+
+    #region Header PLAYER
+    [Space(10)]
+    [Header("PLAYER")]
+    #endregion Header PLAYER
+    #region Tooltip
+    [Tooltip("Player details list - populate the list with the playerdetails scriptable objects")]
+    #endregion Tooltip
+    public List<PlayerDetailsSO> playerDetailsList;
+}
