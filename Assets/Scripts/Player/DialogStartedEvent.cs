ce51ef4d19ee83b4327e3e6a014f12cdb62f2095
Анна Петросян <apetrosia@sfedu.ru>
2022-10-14 21:47:56 +0300

    First commit

---
+++ Assets/Scripts/Player/DialogStartedEvent.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[DisallowMultipleComponent]
+public class DialogStartedEvent : MonoBehaviour
+{
+    public event Action<DialogStartedEvent, DialogStartedEventArgs> OnStartDialog;
+
+    public void CallDialogStartedEvent()
+    {
+        OnStartDialog?.Invoke(this, new DialogStartedEventArgs());
+    }
+}
+
+
+public class DialogStartedEventArgs : EventArgs
+{
+    // TODO
+}
---
+++ Assets/Scripts/Player/PlayerDetailsSO.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerDetails_", menuName = "Scriptable Objects/Player/Player Details")]
+public class PlayerDetailsSO : ScriptableObject
+{
+    #region Header PLAYER BASE DETAILS
+    [Space(10)]
+    [Header("PLAYER BASE DETAILS")]
+    #endregion
+    #region Tooltip
+    [Tooltip("Prefab gameobject for the player")]
+    #endregion
+    public GameObject playerPrefab;
+}
