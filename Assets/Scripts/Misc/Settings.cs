63a1ee9114801f7107ddf7b804c7b5397c936056
AlexanderZemlyak <zemlyaksanya@mail.ru>
2022-10-24 12:21:35 +0300

    Add Enemy classes (including EnemyMovementAI)
    Movement is not working yet. Needs to be configured.

--- Assets/Scripts/Misc/Settings.cs
+++ Assets/Scripts/Misc/Settings.cs
@@ -8,6 +8,22 @@ public static class Settings
     
     // Animator parameters - Dialog
     public static int spaceOpen = Animator.StringToHash("spaceOpen");
-    
+
     #endregion ANIMATOR PARAMETERS
+
+    #region ASTAR PATHFINDING PARAMETERS
+    public const int defaultGridNodesWidthForPathBuilding = 100;
+    public const int defaultGridNodesHeightForPathBuilding = 100;
+
+    public const int defaultAStarMovementPenalty = 40;
+    public const int preferredPathAStarMovementPenalty = 1;
+    public const int targetFrameRateToSpreadPathfindingOver = 60;
+    public const float playerMoveDistanceToRebuildPath = 3f;
+    public const float enemyPathRebuildCooldown = 2f;
+
+    #endregion
+
+    #region ENEMY PARAMETERS
+    public const int defaultEnemyHealth = 50;
+    #endregion
 }
