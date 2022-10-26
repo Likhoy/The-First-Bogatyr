63082426a42d92c0d83fb152049f47bb4516def1
Xuzker <gvozdikov.vyacheslav@yandex.ru>
2022-10-26 14:20:07 +0300

    Update add new scripts

---
+++ Assets/Scripts/Filder/EnemyDetailsSO.cs
@@ -0,0 +1,13 @@
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "FielderMovement", menuName = "Assets/Scripts/Filder/FielderDetails_.asset")]
+public class EnemyDetailsSO : ScriptableObject
+{
+   #region Header BASE ENEMY DETAILS
+   [Space(10)]
+   [Header("BASE ENEMY DETAILS")]
+   #endregion
+}
+*/
