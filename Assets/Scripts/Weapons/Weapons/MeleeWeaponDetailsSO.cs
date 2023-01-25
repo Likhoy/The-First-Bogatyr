28baa88b13e7db6a7e06cc870dcca4c9482e7d52
AlexanderZemlyak <zemlyaksanya@mail.ru>
2023-01-25 15:28:01 +0300

    Return animation code

--- Assets/Scripts/Weapons/Weapons/MeleeWeaponDetailsSO.cs
+++ Assets/Scripts/Weapons/Weapons/MeleeWeaponDetailsSO.cs
@@ -16,13 +16,22 @@ public class MeleeWeaponDetailsSO : ScriptableObject
     #endregion Tooltip
     public Sprite weaponSprite;
 
+    #region Tooltip
+    [Tooltip("Populate with weapon strike animation time")]
+    #endregion Tooltip
+    public float weaponStrikeTime;
+    #region Tooltip
+    [Tooltip("Select weapon cooldown time")]
+    #endregion Tooltip
+    public float weaponCooldownTime;
+
     #region Validation
 #if UNITY_EDITOR
 
     private void OnValidate()
     {
         HelperUtilities.ValidateCheckEmptyString(this, nameof(weaponName), weaponName);
-        
+
     }
 
 #endif
