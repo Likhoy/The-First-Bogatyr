7835f37053e5cf762625d2d3390029ea10f8dc56
AlexanderZemlyak <zemlyaksanya@mail.ru>
2023-03-18 13:27:09 +0300

    Start setting up second quest

--- Assets/Scripts/Scenes/LocationDetailsSO.cs
+++ Assets/Scripts/Scenes/LocationDetailsSO.cs
@@ -10,5 +10,5 @@ public class LocationDetailsSO : ScriptableObject
     #endregion
 
     [Tooltip("TODO")]
-    public EnemySpawnData[] enemiesToSpawnArray;
+    public EnemySpawnData[] enemiesToSpawnImmediately;
 }
