c6f4a97ad428e86ddbd222be4add27cf7416afa7
Анна Петросян <apetrosia@sfedu.ru>
2023-04-02 19:27:10 +0300

    Create Map

--- Assets/Scripts/UI/OpenBestiariy.cs
+++ Assets/Scripts/UI/OpenBestiariy.cs
@@ -6,10 +6,15 @@ public class OpenBestiariy : MonoBehaviour
 {
     [SerializeField]
     GameObject bestiary;
+    [SerializeField]
+    GameObject map;
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.B))
             bestiary.SetActive(!bestiary.activeInHierarchy);
+
+        if (Input.GetKeyDown(KeyCode.M))
+            map.SetActive(!map.activeInHierarchy);
     }
 }
