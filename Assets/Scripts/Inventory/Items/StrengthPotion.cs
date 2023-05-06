4cd7281af325e93c9ae1abb01fa6409a64bbcae1
Ilya Lisitskiy <ilyalisi7skiy@gmail.com>
2023-05-06 23:02:58 +0300

    Set timer to strength potion
    When you use strength potion, you will get X2 damage for 30 seconds (if u have weapon)

--- Assets/Scripts/Inventory/Items/StrengthPotion.cs
+++ Assets/Scripts/Inventory/Items/StrengthPotion.cs
@@ -10,26 +10,30 @@ public class StrengthPotion : Item  {
     override protected void Start() {
         base.Start();
         _player = GameManager.Instance.GetPlayer();
-        _strengthDuration = 5;
+        _strengthDuration = 30;
+
+        // увеличиваю дамаг на 2
+        if (_player.activeWeapon.GetCurrentWeapon() != null) // если есть оружие - увеличиваем урон
+        {
+            if (_player.activeWeapon.GetCurrentWeapon() is MeleeWeapon)
+            {
+                (_player.activeWeapon.GetCurrentWeapon() as MeleeWeapon).weaponDetails.weaponMinDamage *= 2;
+                (_player.activeWeapon.GetCurrentWeapon() as MeleeWeapon).weaponDetails.weaponMaxDamage *= 2;
+            }
+        }
+
     }
 
     private void Update() {
         _strengthTimer -= Time.deltaTime;
     }
 
-    private IEnumerator HealerEffectRoutine() {
-        while (_strengthTimer >= 0f) {
-            // увеличиваю дамаг на 2
-            (_player.activeWeapon.GetCurrentWeapon() as MeleeWeapon).weaponDetails.weaponMinDamage *= 2;
-            (_player.activeWeapon.GetCurrentWeapon() as MeleeWeapon).weaponDetails.weaponMaxDamage *= 2;
-            yield return new WaitForSeconds(1f);
-        }
-    }
-
     override public void UseItem() {
-        // AnimateDrinking();
-        _strengthTimer = _strengthDuration;
-        StopAllCoroutines();
-        StartCoroutine(HealerEffectRoutine());
+        if (_player.activeWeapon.GetCurrentWeapon() != null)
+        {
+            _strengthTimer += _strengthDuration;
+            StartCoroutine(ItemEffects.DamageEffectRoutine(_strengthDuration));
+        }
+
     }
 }
