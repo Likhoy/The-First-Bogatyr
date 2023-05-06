1d6086b2c28b1063e872975132b1a18671c68f16
AlexanderZemlyak <zemlyaksanya@mail.ru>
2023-05-07 00:16:06 +0300

    Edit scenes

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -16,7 +16,9 @@ public class MainMenu : MonoBehaviour
         if (!SaveSystem.HasSavedGameInSlot(2))
             continueButton.SetActive(false);
         controller = FindObjectOfType<DialogueSystemController>();
-        controller.transform.GetChild(0).gameObject.SetActive(false);
+        controller.transform.GetChild(0).GetChild(1).gameObject.SetActive(false);
+        controller.transform.GetChild(0).GetChild(2).gameObject.SetActive(false);
+        controller.transform.GetChild(0).GetChild(3).gameObject.SetActive(false);
         controller.transform.GetChild(1).gameObject.SetActive(false);
     }
 
@@ -29,7 +31,9 @@ public class MainMenu : MonoBehaviour
         else if (SaveSystem.HasSavedGameInSlot(1))
             SaveSystem.LoadFromSlot(1);
 
-        controller.transform.GetChild(0).gameObject.SetActive(true);
+        controller.transform.GetChild(0).GetChild(1).gameObject.SetActive(true);
+        controller.transform.GetChild(0).GetChild(2).gameObject.SetActive(true);
+        controller.transform.GetChild(0).GetChild(3).gameObject.SetActive(true);
         controller.transform.GetChild(1).gameObject.SetActive(true);
     }
 
@@ -38,10 +42,6 @@ public class MainMenu : MonoBehaviour
         SaveSystem.DeleteSavedGameInSlot(2);
         SaveSystem.RestartGame("MainScene");
         controller.ResetDatabase();
-        /*if (SaveSystem.HasSavedGameInSlot(1))
-            SaveSystem.LoadFromSlot(1);
-        else*/
-            //SceneManager.LoadScene("MainScene");
     }
 
     public void ExitPressed()
