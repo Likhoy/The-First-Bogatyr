4aa4577fcd9dc0f63c7399ebadceb7b34bb9d310
AlexanderZemlyak <zemlyaksanya@mail.ru>
2023-08-07 14:28:54 +0300

    Create throwing weapon class (for spear) / 3rd iteration
    Adjust spear spawning position and make it rotate on the z-axis to simulate hinged trajectory.

--- Assets/Scripts/Utilities/HelperUtilities.cs
+++ Assets/Scripts/Utilities/HelperUtilities.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public static class HelperUtilities
 {
@@ -12,7 +13,8 @@ public static class HelperUtilities
     {
         if (mainCamera == null) mainCamera = Camera.main;
 
-        Vector3 mouseScreenPosition = Input.mousePosition;
+        //Vector3 mouseScreenPosition = Input.mousePosition;
+        Vector3 mouseScreenPosition = Mouse.current.position.ReadValue();
 
         // Clamp mouse position to screen size
         mouseScreenPosition.x = Mathf.Clamp(mouseScreenPosition.x, 0f, Screen.width);
