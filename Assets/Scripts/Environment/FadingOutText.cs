a6bc4204b67a10ee96505b2f89db827382838e32
AlexanderZemlyak <zemlyaksanya@mail.ru>
2023-08-11 15:53:05 +0300

    Fix bug with Shift hint
    Not sure if all cases are taken into consideration still in FadingOutText script.

--- Assets/Scripts/Environment/FadingOutText.cs
+++ Assets/Scripts/Environment/FadingOutText.cs
@@ -1,16 +1,33 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
+using Utils;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class FadingOutText : MonoBehaviour
 {
+    private struct FadingOutRoutineInfo
+    {
+        public string textToShow;
+        public float delayDuration;
+        public float queryTime;
+
+        public FadingOutRoutineInfo(string textToShow, float delayDuration, float queryTime)
+        {
+            this.textToShow = textToShow;
+            this.delayDuration = delayDuration;
+            this.queryTime = queryTime;
+        }
+    }
+
     public string TextToShow { get; set; } = "";
     private TextMeshProUGUI TMPro;
-    private float alphaDecrease = 0.05f;
-    private WaitForSeconds alphaDecreaseCooldown = new WaitForSeconds(0.1f);
+
+    private const float alphaDecrease = 0.05f;
+    private readonly WaitForSeconds alphaDecreaseCooldown = new WaitForSeconds(0.1f);
+    private Coroutine currentFadingOutRoutine;
+    private PriorityQueue<FadingOutRoutineInfo, float> fadingOutRoutineQueue = new PriorityQueue<FadingOutRoutineInfo, float>();
+
+    private const float timeBeforeFadingOut = 3f;
 
     private void Awake()
     {
@@ -19,26 +36,59 @@ public class FadingOutText : MonoBehaviour
 
     public void ShowHint(float delayDuration)
     {
-        StartCoroutine(FadingOutRoutine(delayDuration));
+        if (fadingOutRoutineQueue.Count == 0)
+        {
+            fadingOutRoutineQueue.Enqueue(new FadingOutRoutineInfo(TextToShow, delayDuration, Time.time), delayDuration);
+            currentFadingOutRoutine = StartCoroutine(FadingOutRoutine(delayDuration));
+        }  
+        else
+        {
+            FadingOutRoutineInfo oldInfo = fadingOutRoutineQueue.Peek();
+            // if new delayDuration less than time before old hint appearance then interrupt old routine
+            if (delayDuration < oldInfo.delayDuration - (Time.time - oldInfo.queryTime))
+            {
+                StopCoroutine(currentFadingOutRoutine);
+                fadingOutRoutineQueue.Dequeue();
+                fadingOutRoutineQueue.Enqueue(new FadingOutRoutineInfo(TextToShow, delayDuration, Time.time), delayDuration);
+                currentFadingOutRoutine = StartCoroutine(FadingOutRoutine(delayDuration));
+                fadingOutRoutineQueue.Enqueue(oldInfo, delayDuration + 1f); // make old routine second after new
+            }
+            else if (delayDuration < oldInfo.delayDuration)
+            {
+                fadingOutRoutineQueue.Dequeue(); // dequeue current routine info to make new routine second after it
+                fadingOutRoutineQueue.Enqueue(new FadingOutRoutineInfo(TextToShow, delayDuration, Time.time), delayDuration);
+            }
+            else
+            {
+                fadingOutRoutineQueue.Enqueue(new FadingOutRoutineInfo(TextToShow, delayDuration, Time.time), delayDuration);
+            }
+        } 
     }
 
 
     private IEnumerator FadingOutRoutine(float delayDuration)
     {
-        string textToShow = TextToShow;
+        FadingOutRoutineInfo info = fadingOutRoutineQueue.Peek();
         if (delayDuration > 0)
             yield return new WaitForSeconds(delayDuration);
-        TMPro.text = textToShow;
-        yield return new WaitForSeconds(3f);
-        if (textToShow == TMPro.text)
+        
+        TMPro.text = info.textToShow;
+        yield return new WaitForSeconds(timeBeforeFadingOut);
+
+        while (TMPro.alpha > 0f)
         {
-            while (TMPro.alpha > 0f)
-            {
-                TMPro.alpha -= alphaDecrease;
-                yield return alphaDecreaseCooldown;
-            }
-            TMPro.alpha = 1f;
-            TMPro.text = "";
+            TMPro.alpha -= alphaDecrease;
+            yield return alphaDecreaseCooldown;
         }
+        TMPro.alpha = 1f;
+        TMPro.text = "";
+        if (info.textToShow == fadingOutRoutineQueue.Peek().textToShow)
+            fadingOutRoutineQueue.Dequeue();
+
+        if (fadingOutRoutineQueue.Count > 0)
+        {
+            FadingOutRoutineInfo newInfo = fadingOutRoutineQueue.Peek();
+            currentFadingOutRoutine = StartCoroutine(FadingOutRoutine(newInfo.delayDuration - (Time.time - newInfo.queryTime)));
+        }    
     }
 }
