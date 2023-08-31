0097cc036550f4334902445a1213dd2ff4f4fa13
Daria-Romanko <115109486+Daria-Romanko@users.noreply.github.com>
2023-08-31 18:37:52 +0300

    Update remaining ammo counter in WeaponUI

--- Assets/Scripts/UI/WeaponUI.cs
+++ Assets/Scripts/UI/WeaponUI.cs
@@ -10,16 +10,43 @@ public class WeaponUI : MonoBehaviour
     private void OnEnable()
     {
         GameManager.Instance.GetPlayer().setActiveWeaponEvent.OnSetActiveWeapon += WeaponEvent_OnWeaponChanged;
+        GameManager.Instance.GetPlayer().weaponFiredEvent.OnWeaponFired += FiredIvent_OnWeaponFired;
     }
 
     private void OnDisable()
     {
         GameManager.Instance.GetPlayer().setActiveWeaponEvent.OnSetActiveWeapon -= WeaponEvent_OnWeaponChanged;
+        GameManager.Instance.GetPlayer().weaponFiredEvent.OnWeaponFired -= FiredIvent_OnWeaponFired;
     }
 
     private void WeaponEvent_OnWeaponChanged(SetActiveWeaponEvent setActiveWeaponEvent, SetActiveWeaponEventArgs setActiveWeaponEventArgs)
     {
         ShowWeaponSprite(setActiveWeaponEventArgs);
+    }
+
+    private void FiredIvent_OnWeaponFired(WeaponFiredEvent weaponFiredEvent, WeaponFiredEventArgs weaponFiredEventArgs)
+    {
+        ShowWeaponText(weaponFiredEventArgs);
+    }
+
+    private void ShowWeaponText(WeaponFiredEventArgs weaponFiredEventArgs)
+    {
+        if(weaponFiredEventArgs.weapon != null)
+        {
+            if(weaponFiredEventArgs.weapon is RangedWeapon)
+            {
+                countItems.text = "" + (weaponFiredEventArgs.weapon as RangedWeapon).weaponRemainingAmmo;
+            }
+            else
+            {
+                countItems.text = "";
+            }
+        }
+        else
+        {
+            countItems.text = "";
+        }
+        
     }
 
     private void ShowWeaponSprite(SetActiveWeaponEventArgs setActiveWeaponEventArgs)
