0e578e329251d6143eed812a6387136eff711cf6
Daria-Romanko <Diagic1000@gmail.com>
2023-11-09 15:56:12 +0300

    Add Bonus UI Controller

---
+++ Assets/Scripts/UI/BonusesUI/BonusUIController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BonusUIController : MonoBehaviour
+{
+    public GameObject bonusPanel;
+    public Button[] bonusButtons;
+    public GameObject bonusDescription;
+
+    private List<BonusDetailsSO> randomBonuses;
+
+    private void OnEnable()
+    {
+        StaticEventHandler.OnWaveFinished += ShowBonusPanel;
+    }
+
+    private void OnDisable()
+    {
+        StaticEventHandler.OnWaveFinished -= ShowBonusPanel;
+    }
+
+    private void ShowBonusPanel(WaveFinishedEventArgs obj)
+    {
+        randomBonuses = BonusHandler.GetRandomBonusesForWave(obj.waveNumber);
+
+        for (int i = 0; i < bonusButtons.Length; i++)
+        {
+            bonusButtons[i].onClick.AddListener(() => ActivateBonus(i));
+            bonusButtons[i].image.overrideSprite = randomBonuses[i].bonusSprite;
+        }
+
+        bonusPanel.SetActive(true);
+    }
+
+    private void ActivateBonus(int index)
+    {
+        BonusDetailsSO bonusDetails = randomBonuses[index];
+        BonusHandler.ApplyBonus(bonusDetails);
+        Debug.Log("Activated bonus: " + bonusDetails.bonusName);
+    }
+
+    public void OnButtonHover(int index)
+    {
+        bonusDescription.SetActive(true);
+
+        BonusDetailsSO bonusDetails = randomBonuses[index];
+        bonusDescription.GetComponentInChildren<TMP_Text>().text = bonusDetails.bonusName;
+    }
+
+    public void OnButtonHoverExit()
+    {
+        bonusDescription.SetActive(false);
+    }
+}
