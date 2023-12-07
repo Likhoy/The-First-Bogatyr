c12f1f7283bd1ff61d94506585978b6625f9d04b
AlexanderZemlyak <zemlyaksanya@mail.ru>
2023-12-07 19:42:36 +0300

    Fix bug with health hint

--- Assets/Scripts/Scenes/CustomSceneTransitionManager.cs
+++ Assets/Scripts/Scenes/CustomSceneTransitionManager.cs
@@ -5,38 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class CustomSceneTransitionManager : StandardSceneTransitionManager
 {
-    /*[SerializeField] private string leavingSceneName;
-    [SerializeField] private string enteringSceneName;*/
-
-    //private string previousSceneName;
     public bool areScenesCorrect = false;
-
-    /*private void OnEnable()
-    {
-        SceneManager.sceneUnloaded += SceneManager_sceneUnloaded;
-        SceneManager.activeSceneChanged += SceneManager_activeSceneChanged;
-    }
-
-    private void OnDisable()
-    {
-        SceneManager.sceneUnloaded -= SceneManager_sceneUnloaded;
-        SceneManager.activeSceneChanged -= SceneManager_activeSceneChanged;
-    }
-
-    private void SceneManager_sceneUnloaded(Scene previousScene)
-    {
-        previousSceneName = previousScene.name;
-    }
-
-    private void SceneManager_activeSceneChanged(Scene previousScene, Scene nextScene)
-    {
-        if (previousSceneName == leavingSceneName && nextScene.name == enteringSceneName)
-        {
-            areScenesCorrect = true;
-        }
-        else
-            areScenesCorrect = false;
-    }*/
 
     public override IEnumerator LeaveScene()
     {
