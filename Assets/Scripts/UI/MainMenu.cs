dba0b48f1ca5b38c1bffe27aa0d8f130d8cc800f
AlexanderZemlyak <zemlyaksanya@mail.ru>
2024-09-07 18:52:42 +0300

    Add define checks in MainMenu to achieve correct behaviour in build

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -21,15 +21,21 @@ public class MainMenu : MonoBehaviour
         
         DialogueManager.Instance.gameObject.SetActive(true); // might be disabled in endless mode
 
+#if UNITY_EDITOR
         DialogueManager.Instance.transform.GetChild(0).GetChild(1).gameObject.SetActive(false);
+#endif
     }
 
     private void Start()
     {
         allButtons = new GameObject[5] { continueButton, newGameButton, endlessModeGameButton, settingsButton, exitButton };
 
+#if !UNITY_EDITOR
+        DialogueManager.Instance.transform.GetChild(0).GetChild(1).gameObject.SetActive(false);
+#endif
+
         // this way of placing disabling of dialogueManager children is intentional
-		DialogueManager.Instance.transform.GetChild(0).GetChild(2).gameObject.SetActive(false);
+        DialogueManager.Instance.transform.GetChild(0).GetChild(2).gameObject.SetActive(false);
         DialogueManager.Instance.transform.GetChild(0).GetChild(3).gameObject.SetActive(false);
         DialogueManager.Instance.transform.GetChild(1).gameObject.SetActive(false);
     }
