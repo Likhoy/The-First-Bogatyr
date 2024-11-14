d4afa4ce373080e81c5729db435d9c65863973a7
Daria-Romanko <115109486+Daria-Romanko@users.noreply.github.com>
2024-11-14 22:19:03 +0300

    Change menu, settings (add gamma setting)

--- Assets/Scripts/UI/SettingsMenu.cs
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,40 +6,87 @@ using UnityEngine.SceneManagement;
 using TMPro;
 using UnityEngine.Audio;
 using System;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+using UnityEditor;
 
 public class SettingsMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject settingsPanel;
+    [SerializeField] private GameObject audioSettingsPanel;
+    [SerializeField] private GameObject videoSettingsPanel;
+    [SerializeField] private GameObject controlsSettingsPanel;   
 
-    private Toggle AudioToggle;
+    [SerializeField] private Toggle fulscreeenToggle;
+    [SerializeField] private TMP_Dropdown resolutionDropdown;
+    private Resolution[] resolutions;
+    private List<Resolution> filteredResolutions;
+    private float currentRefreshRate;
+    private int currentResolutionIndex = 0;
 
-    public TMP_Dropdown resolutionDropdown;
+    [SerializeField] private AudioMixer theMixer;
+    [SerializeField] private Toggle audioToggle;
+    [SerializeField] private TMP_Text mastLabel, musicLabel, sfxLabel;
+    [SerializeField] private Slider mastSlider, musicSlider, sfxSlider;
 
-    public AudioMixer theMixer;
-
-    public TMP_Text mastLabel, musicLabel, sfxLabel;
-    public Slider mastSlider, musicSlider, sfxSlider;
-
-    public bool volumeOn = false;
-
-    Resolution[] resolutions;
+    [SerializeField] private Slider gammaSlider;
+    [SerializeField] private TMP_Text gammaLabel;
+    [SerializeField] private VolumeProfile profile;
+    private LiftGammaGain volumeGain;
 
     void Start()
     {
+        InitializeFullscreenToggleSettings();
+        InitializeResolutionSettings();
+        InitializeGammaSettings();
+        InitializeAudioToggleSettings();
+        InitializeAudioSlidersSettings();
+    }
 
+    private void InitializeFullscreenToggleSettings()
+    {
+        fulscreeenToggle.onValueChanged.AddListener(SetFullscreen);
+
+        if (PlayerPrefs.HasKey("Fullscreen"))
+        {
+            fulscreeenToggle.isOn = PlayerPrefs.GetInt("Fullscreen") == 1;
+        }
+        else
+        {
+            fulscreeenToggle.isOn = true;
+        }
+    }
+
+    private void InitializeResolutionSettings()
+    {
         resolutions = Screen.resolutions;
+        filteredResolutions = new List<Resolution>();
 
         resolutionDropdown.ClearOptions();
+        currentRefreshRate = (float)Screen.currentResolution.refreshRateRatio.value;
 
-        List<string> options = new List<string>();
-
-        int currentResolutionIndex = 0;
         for (int i = 0; i < resolutions.Length; i++)
         {
-            string option = resolutions[i].width + "x" + resolutions[i].height + " " + resolutions[i].refreshRateRatio + "Hz";
-            options.Add(option);
+            if ((float)resolutions[i].refreshRateRatio.value == currentRefreshRate)
+            {
+                filteredResolutions.Add(resolutions[i]);
+            }
+        }
 
-            if (resolutions[i].width == Screen.currentResolution.width &&
-                resolutions[i].height == Screen.currentResolution.height)
+        filteredResolutions.Sort((a, b) =>
+        {
+            if (a.width != b.width)
+                return b.width.CompareTo(a.width);
+            else
+                return b.height.CompareTo(a.height);
+        });
+
+        List<string> options = new List<string>();
+        for (int i = 0; i < filteredResolutions.Count; i++)
+        {
+            string resolutionOption = filteredResolutions[i].width + "x" + filteredResolutions[i].height;
+            options.Add(resolutionOption);
+            if (filteredResolutions[i].width == Screen.width && filteredResolutions[i].height == Screen.height && (float)filteredResolutions[i].refreshRateRatio.value == currentRefreshRate)
             {
                 currentResolutionIndex = i;
             }
@@ -48,6 +95,47 @@ public class SettingsMenu : MonoBehaviour
         resolutionDropdown.AddOptions(options);
         resolutionDropdown.value = currentResolutionIndex;
         resolutionDropdown.RefreshShownValue();
+        SetResolution(currentResolutionIndex);
+
+        resolutionDropdown.onValueChanged.AddListener(SetResolution);
+    }
+
+    private void InitializeGammaSettings()
+    {        
+        profile.TryGet<LiftGammaGain>(out volumeGain);
+
+        gammaSlider.onValueChanged.AddListener(SetGamma);
+
+        if (PlayerPrefs.HasKey("GammaValue"))
+        {
+            float gammaValue = PlayerPrefs.GetFloat("GammaValue");
+            gammaSlider.value = gammaValue;
+        }
+        else
+        {
+            gammaSlider.value = volumeGain.gamma.value.x;
+        }
+    }
+
+    private void InitializeAudioToggleSettings()
+    {
+        audioToggle.onValueChanged.AddListener(SetVolumeMute);
+
+        if (PlayerPrefs.HasKey("VolumeMute"))
+        {
+            audioToggle.isOn = PlayerPrefs.GetInt("VolumeMute") == 1;
+        }
+        else
+        {
+            audioToggle.isOn = true;
+        }
+    }
+
+    private void InitializeAudioSlidersSettings()
+    {
+        mastSlider.onValueChanged.AddListener(SetMasterVol);
+        musicSlider.onValueChanged.AddListener(SetMusicVol);
+        sfxSlider.onValueChanged.AddListener(SetSFXVol);
 
         if (PlayerPrefs.HasKey("MasterVol"))
         {
@@ -75,32 +163,30 @@ public class SettingsMenu : MonoBehaviour
         {
             sfxSlider.value = 0f;
         }
-             
+
         mastLabel.text = Mathf.RoundToInt(mastSlider.value * 100).ToString();
         musicLabel.text = Mathf.RoundToInt(musicSlider.value * 100).ToString();
         sfxLabel.text = Mathf.RoundToInt(sfxSlider.value * 100).ToString();
     }
 
-
     public void SetResolution(int resolutionIndex)
     {
-        Resolution resolution = resolutions[resolutionIndex];
+        Resolution resolution = filteredResolutions[resolutionIndex];
         Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+
+        PlayerPrefs.SetInt("ResolutionIndex", resolutionIndex);
     }
 
-
-    public void SetMasterVol()
+    public void SetMasterVol(float value)
     {
         mastLabel.text = Mathf.RoundToInt(mastSlider.value * 100).ToString();
 
-        theMixer.SetFloat("MasterVol", Mathf.Log10(mastSlider.value) * 20);
+        theMixer.SetFloat("MasterVol", Mathf.Log10(value) * 20);
 
-        PlayerPrefs.SetFloat("MasterVol", mastSlider.value);
-
-        PlayerPrefs.Save();
+        PlayerPrefs.SetFloat("MasterVol", value);
     }
 
-    public void SetMusicVol()
+    public void SetMusicVol(float value)
     {
         musicLabel.text = Mathf.RoundToInt(musicSlider.value * 100).ToString();
 
@@ -109,7 +195,7 @@ public class SettingsMenu : MonoBehaviour
         PlayerPrefs.SetFloat("MusicVol", musicSlider.value);
     }
 
-    public void SetSFXVol()
+    public void SetSFXVol(float value)
     {
         sfxLabel.text = Mathf.RoundToInt(sfxSlider.value * 100).ToString();
 
@@ -118,19 +204,50 @@ public class SettingsMenu : MonoBehaviour
         PlayerPrefs.SetFloat("SFXVol", sfxSlider.value);
     }
 
-    public void Mute()
-
+    public void SetFullscreen(bool isFullscreen)
     {
-        if (volumeOn)
+        Screen.fullScreen = isFullscreen;
+
+        PlayerPrefs.SetInt("Fullscreen", isFullscreen ? 1 : 0);
+    }
+
+    public void SetVolumeMute(bool isMute)
+    {
+        if (isMute)
         {
-            volumeOn = false;
-            AudioListener.volume = 0;
+            AudioListener.volume = 1;
         }
         else
         {
-            volumeOn = true;
-            AudioListener.volume = 1;
-
+            AudioListener.volume = 0;
         }
+
+        PlayerPrefs.SetInt("VolumeMute", isMute ? 1 : 0);
+    }
+
+    public void SetGamma(float value)
+    {
+        if (volumeGain != null)
+        {
+            volumeGain.gamma.value = new Vector4(value, value, value, value);            
+            PlayerPrefs.SetFloat("GammaValue", value);
+
+            gammaLabel.text = $"{value + 1:F2}";
+        }
+    }
+
+    public void HideSettings()
+    {
+        PlayerPrefs.Save();
+
+        audioSettingsPanel.SetActive(false);
+        videoSettingsPanel.SetActive(false);
+        controlsSettingsPanel.SetActive(false);
+        settingsPanel.SetActive(false);
+    }
+
+    public void ShowSettings()
+    {
+        settingsPanel.SetActive(true);
     }
 }
